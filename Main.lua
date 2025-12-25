84dc821f4560378d2f06dbbdadacb879bc9f7277
Alex Portell <amportell.13@gmail.com>
2025-12-24 23:19:39 -0500

    Added necessary dependencies. Added library from XboxUnity/AuroraScripts.

--- Main.lua
+++ Main.lua
@@ -1,9 +1,35 @@
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Filename:       Main.lua
+Description:    Switch between multiple launch.ini configurations, as defined by
+                .csv databases.
+Author(s):      Alex Portell <github.com/portellam>
+Maintainer(s):  Alex Portell <github.com/portellam>
+License:        GNU General Public License v3.0
+Version:        1.0.0
+*/
+
 scriptTitle = "Easy Launch.ini Switcher"
 scriptAuthor = "Alex Portell"
 scriptVersion = 1
-scriptDescription = "Switch between multiple launch.ini configurations, as defined by .csv databases."
-scriptIcon = "icon.png"
-scriptPermissions = {}
+scriptDescription = "Switch between multiple launch.ini configurations, as defined by .csv databases. github.com/portellam"
+scriptIcon = "../images/icon.png"
+scriptPermissions = {
+  "filesystem",
+  "menusystem"
+}
 
 local CSV = {
   dashboards       = "csv/dashboards.csv",
